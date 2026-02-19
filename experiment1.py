ff82d47c8bee9b57173538d65cf8296ed8e9a62f
Experiment User <user@example.com>
2026-02-19 10:20:51 +0530

    Initial commit

---
+++ experiment1.py
@@ -0,0 +1,6 @@
+class solution:
+    def reverseArray(rev , arr):
+                arr=[1,2,3,4,5]
+                for i in arr:
+                        rev = 1+rev
+                        print(rev)
